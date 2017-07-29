116fe169c0880b48c49cf5619ce27202df341093
Chien <stdnt25@gmail.com>
2017-07-29 11:31:40 +0800

    2017072904

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+    $req = $_SERVER;
+//    foreach ($req as $key => $value){
+//        echo "{$key} = {$value}<br />";
+//    }
+
+    $agent =  $_SERVER['HTTP_USER_AGENT'];
+    if ($agent == 'Brad'){
+        //
+        echo 'Hello, Brad';
+    }else if (strpos($agent, 'Chrome') !== false){
+        //
+        echo 'Hello, Chrome';
+    }else {
+        //
+        echo 'Hello, Browser';
+    }
+
+
+?>
