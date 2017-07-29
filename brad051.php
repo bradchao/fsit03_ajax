258d8904327c7b070991f9b58188e2b814600460
Chien <stdnt25@gmail.com>
2017-07-29 14:21:37 +0800

    2017072906

---
+++ brad051.php
@@ -0,0 +1,9 @@
+<?php
+    header('Content-Type: text/xml; charset="UTF-8"');
+?>
+<note>
+  <to>歐</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+</note>
